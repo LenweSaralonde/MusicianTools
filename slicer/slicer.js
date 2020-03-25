d54921c6bdf78843bc59149ce66e161ce04e72ae
LenweSaralonde <lenwe@lenwe.info>
2020-03-25 18:47:12 +0100

    Reworked slicer. Normalization is now optional.

--- slicer/slicer.js
+++ slicer/slicer.js
@@ -7,79 +7,96 @@
 
 const fs = require('fs');
 const child_process = require('child_process');
-const argv = require('minimist')(process.argv.slice(2));
 
-if (argv['_'][0] === undefined) {
-	console.error("Usage: node slicer.js [-l <normalize level>] [-n <normalize type>] <wav file>");
-	process.exit(1);
-}
+function main() {
 
-const slicesFile = argv['_'][0];
-const slicesDir = slicesFile.replace(/\.[^\.]+$/, '');
+	const argv = require('minimist')(process.argv.slice(2));
 
-if (!fs.existsSync(slicesFile)) {
-	console.error("Error: " + slicesFile + " does not exist.");
-	process.exit(1);
-}
+	if (argv['_'][0] === undefined) {
+		console.error("Usage: node slicer.js [-l <normalize level>] [-n <normalize type>] <wav file>");
+		process.exit(1);
+	}
 
-try {
-	fs.mkdirSync(slicesDir);
-} catch (e) {}
+	const slicesFile = argv['_'][0];
+	const slicesDir = slicesFile.replace(/\.[^\.]+$/, '');
 
-const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B'];
-let NOTES_FROM = argv.from || 'C0';
-let NOTES_TO   = argv.to || 'C8';
-let NOTE_DURATION = (argv.d !== undefined)?parseInt(argv.d):6;
-let FFMPEG_PARAMS = '';
-let SLICE_FORMAT = argv.format || 'ogg';
-let NORMALIZE_LEVEL = (argv.l !== undefined)?argv.l:-10;
-let NORMALIZE_TYPE = (argv.n !== undefined)?argv.n:'peak';
-
-const notesFromMatches = NOTES_FROM.match(/([A-Z#]+)([0-9-]+)/);
-
-let fileIndex = 0;
-let noteIndex = NOTES.indexOf(notesFromMatches[1]);
-let currentOctave = notesFromMatches[2];
-let currentNote;
-let ffmpegCommand;
-
-const normalizedFile = slicesDir + '_normalized.ogg';
-
-const outputFiles = [];
-
-do {
-	currentNote = NOTES[noteIndex] + currentOctave;
-	const outputFile = slicesDir + '/' + currentNote + '.' + SLICE_FORMAT;
-	let out;
-
-	// Extract sample
-	ffmpegCommand = `ffmpeg -i ${slicesFile} -y -ss ${fileIndex * NOTE_DURATION} -t ${NOTE_DURATION} __chunk__.wav`;
-	out = child_process.spawnSync(ffmpegCommand, [], { shell: true }).output.toString();
-
-	// Normalize sample
-	ffmpegCommand = `ffmpeg-normalize __chunk__.wav -nt ${NORMALIZE_TYPE} -t ${NORMALIZE_LEVEL} -o __normalized__.wav`;
-	child_process.execSync(ffmpegCommand);
-
-	// Encode sample
-	ffmpegCommand = `ffmpeg -i __normalized__.wav -y ${FFMPEG_PARAMS} ${outputFile}`;
-	child_process.execSync(ffmpegCommand);
+	if (!fs.existsSync(slicesFile)) {
+		console.error("Error: " + slicesFile + " does not exist.");
+		process.exit(1);
+	}
 
 	try {
-		fs.unlinkSync('__chunk__.wav');
-		fs.unlinkSync('__normalized__.wav');
+		fs.mkdirSync(slicesDir);
 	} catch (e) {}
 
-	outputFiles.push(outputFile);
+	const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B'];
+	let NOTES_FROM = argv.from || 'C0';
+	let NOTES_TO   = argv.to || 'C8';
+	let NOTE_DURATION = (argv.d !== undefined)?parseInt(argv.d):6;
+	let FFMPEG_PARAMS = '';
+	let SLICE_FORMAT = argv.format || 'ogg';
+	let NORMALIZE_LEVEL = argv.l;
+	let NORMALIZE_TYPE = argv.n;
+	let NORMALIZE_FROM = argv['normalize-from'];
+	let NORMALIZE_TO = argv['normalize-to'];
 
-	fileIndex++;
-	noteIndex++;
-	if (noteIndex === NOTES.length) {
-		noteIndex = 0;
-		currentOctave++;
-	}
-} while (currentNote !== NOTES_TO)
+	const notesFromMatches = NOTES_FROM.match(/([A-Z#]+)([0-9-]+)/);
 
-// Create normalized source file for comparison
-const concatFiles = outputFiles.join('|');
-ffmpegCommand = `ffmpeg -i "concat:${concatFiles}" -y -acodec copy ${normalizedFile}`;
-child_process.execSync(ffmpegCommand);
+	let fileIndex = 0;
+	let noteIndex = NOTES.indexOf(notesFromMatches[1]);
+	let currentOctave = notesFromMatches[2];
+	let currentNote;
+	let ffmpegCommand;
+
+	const normalizedFile = slicesDir + '_normalized.ogg';
+
+	const outputFiles = [];
+
+	do {
+		currentNote = NOTES[noteIndex] + currentOctave;
+		const outputFile = slicesDir + '/' + currentNote + '.' + SLICE_FORMAT;
+		let out;
+
+		// Extract sample
+		ffmpegCommand = `ffmpeg -i ${slicesFile} -y -ss ${fileIndex * NOTE_DURATION} -t ${NOTE_DURATION} __chunk__.wav`;
+		out = child_process.spawnSync(ffmpegCommand, [], { shell: true }).output.toString();
+
+		let level = 0;
+
+		// Normalize sample
+		if (NORMALIZE_TYPE) {
+			// Measure level to adjust
+			ffmpegCommand = `ffmpeg-normalize __chunk__.wav -n -v -f -nt ${NORMALIZE_TYPE} -t ${NORMALIZE_LEVEL}`;
+			out = child_process.spawnSync(ffmpegCommand, [], { shell: true }).output.toString();
+			const levelMatch = out.match(/INFO: Adjusting stream .+ by (.*) dB to reach/);
+			level = levelMatch && (levelMatch[1] * 1);
+		}
+
+		// Encode sample and adjust level if needed
+		const volumeCommand = level ? `-filter:a "volume=${level}dB"`:'';
+		ffmpegCommand = `ffmpeg -i __chunk__.wav -y ${volumeCommand} ${FFMPEG_PARAMS} ${outputFile}`;
+		const levelDisplay = level ? ('(' + (level > 0 ? '+' : '') + new Intl.NumberFormat('en-US').format(level) + 'dB)') : '';
+		console.log(`Encoding ${outputFile} ${levelDisplay}`);
+		out = child_process.spawnSync(ffmpegCommand, [], { shell: true }).output.toString();
+
+		try {
+			fs.unlinkSync('__chunk__.wav');
+		} catch (e) {}
+
+		outputFiles.push(outputFile);
+
+		fileIndex++;
+		noteIndex++;
+		if (noteIndex === NOTES.length) {
+			noteIndex = 0;
+			currentOctave++;
+		}
+	} while (currentNote !== NOTES_TO)
+
+	// Create normalized source file for comparison
+	const concatFiles = outputFiles.join('|');
+	ffmpegCommand = `ffmpeg -i "concat:${concatFiles}" -y -acodec copy ${normalizedFile}`;
+	const out = child_process.spawnSync(ffmpegCommand, [], { shell: true }).output.toString();
+}
+
+main();
