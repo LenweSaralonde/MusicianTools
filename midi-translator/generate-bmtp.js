02f6feb1ee59050c7f22e0dae5432738fa8c4365
LenweSaralonde <lenwe@lenwe.info>
2020-07-22 00:53:31 +0200

    Create one MIDI translator preset per MIDI channel

--- midi-translator/generate-bmtp.js
+++ midi-translator/generate-bmtp.js
@@ -70,51 +70,57 @@ const START = 36;
 
 let bmtp =
 	`[Project]\n` +
-	`Version=1\n\n` +
-	`[Preset.0]\n` +
-	`Name=Musician MIDI\n` +
-	`Active=1\n`;
+	`Version=1\n`;
 
-let i;
-let rule = 0;
-for (i in MAPPING) {
-	const compKeyCode = MAPPING[i];
-	const midiKey = START + parseInt(i, 10);
-	const midiKeyCode = midiKey.toString(16).toUpperCase();
-	const octave = Math.floor((midiKey - 12) / 12);
-	const note = (midiKey - 12) % 12;
-	const midiKeyName = NOTE_NAMES[note] + octave;
+let c;
+for (c = 0; c < 16; c++) {
+	bmtp +=
+		`\n[Preset.${c}]\n` +
+		`Name=Musician MIDI (channel ${c + 1})\n` +
+		`Active=1\n`;
+
+	const cHex = c.toString(16).toUpperCase();
+	let rule = 0;
+	let i;
+	for (i in MAPPING) {
+		const compKeyCode = MAPPING[i];
+		const midiKey = START + parseInt(i, 10);
+		const midiKeyCode = midiKey.toString(16).toUpperCase();
+		const octave = Math.floor((midiKey - 12) / 12);
+		const note = (midiKey - 12) % 12;
+		const midiKeyName = NOTE_NAMES[note] + octave;
+
+		bmtp +=
+			`Name${rule}=${midiKeyName} up\n` +
+			`Incoming${rule}=MID18${cHex}${midiKeyCode}pp\n` +
+			`Outgoing${rule}=KAM12100KSQ1000${compKeyCode}\n` +
+			`Options${rule}=Actv01Stop00OutO00\n`;
+		rule++;
+		bmtp +=
+			`Name${rule}=${midiKeyName} up (alternate)\n` +
+			`Incoming${rule}=MID19${cHex}${midiKeyCode}00\n` +
+			`Outgoing${rule}=KAM12100KSQ1000${compKeyCode}\n` +
+			`Options${rule}=Actv01Stop01OutO00\n`;
+		rule++;
+		bmtp +=
+			`Name${rule}=${midiKeyName} down\n` +
+			`Incoming${rule}=MID19${cHex}${midiKeyCode}pp\n` +
+			`Outgoing${rule}=KAM11000KSQ1000${compKeyCode}\n` +
+			`Options${rule}=Actv01Stop00OutO00\n`;
+		rule++;
+	}
 
 	bmtp +=
-		`Name${rule}=${midiKeyName} up\n` +
-		`Incoming${rule}=MID180${midiKeyCode}pp\n` +
-		`Outgoing${rule}=KAM12100KSQ1000${compKeyCode}\n` +
+		`Name${rule}=Pedal down\n` +
+		`Incoming${rule}=MID1B${cHex}407F\n` +
+		`Outgoing${rule}=KAM11000KSQ10001020\n` +
 		`Options${rule}=Actv01Stop00OutO00\n`;
 	rule++;
 	bmtp +=
-		`Name${rule}=${midiKeyName} up (alternate)\n` +
-		`Incoming${rule}=MID190${midiKeyCode}00\n` +
-		`Outgoing${rule}=KAM12100KSQ1000${compKeyCode}\n` +
-		`Options${rule}=Actv01Stop01OutO00\n`;
-	rule++;
-	bmtp +=
-		`Name${rule}=${midiKeyName} down\n` +
-		`Incoming${rule}=MID190${midiKeyCode}pp\n` +
-		`Outgoing${rule}=KAM11000KSQ1000${compKeyCode}\n` +
+		`Name${rule}=Pedal up\n` +
+		`Incoming${rule}=MID1B${cHex}4000\n` +
+		`Outgoing${rule}=KAM12100KSQ10001020\n` +
 		`Options${rule}=Actv01Stop00OutO00\n`;
-	rule++;
 }
-
-bmtp +=
-	`Name${rule}=Pedal down\n` +
-	`Incoming${rule}=MID1B0407F\n` +
-	`Outgoing${rule}=KAM11000KSQ10001020\n` +
-	`Options${rule}=Actv01Stop00OutO00\n`;
-rule++;
-bmtp +=
-	`Name${rule}=Pedal up\n` +
-	`Incoming${rule}=MID1B04000\n` +
-	`Outgoing${rule}=KAM12100KSQ10001020\n` +
-	`Options${rule}=Actv01Stop00OutO00\n`;
 
 process.stdout.write(bmtp);
